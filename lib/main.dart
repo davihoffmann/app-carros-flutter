12f0548371dcdc6ace2ec328047df5abb6b7756e
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-03-26 09:45:07 -0300

    criado formulario da tela de login

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        //brightness: Brightness.dark,
+        scaffoldBackgroundColor: Colors.white
       ),
       home: LoginPage(),
     );
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,8 +3,71 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Carros'),
+      ),
+      body: _body(),
+    );
+  }
+
+  _body() {
     return Container(
-      
+      padding: EdgeInsets.all(25),
+      child: ListView(
+        children: <Widget>[
+          Container(
+            child: Image.network(
+              'https://www.automanianet.com.br/wp-content/uploads/11-out-18-App-CarMasters-2.png',
+              height: 150,
+            ),
+          ),
+          SizedBox(height: 10),
+          TextFormField(
+            style: TextStyle(
+              fontSize: 25,
+              color: Colors.blue,
+            ),
+            decoration: InputDecoration(
+              labelText: 'Login',
+              labelStyle: TextStyle(
+                fontSize: 25,
+              ),
+              hintText: 'Digite o Login',
+            ),
+          ),
+          SizedBox(height: 10),
+          TextFormField(
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 25,
+            ),
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Senha',
+              labelStyle: TextStyle(
+                fontSize: 25,
+              ),
+              hintText: 'Digite a Senha',
+            ),
+          ),
+          SizedBox(height: 20),
+          Container(
+            height: 50,
+            child: RaisedButton(
+              color: Colors.blue,
+              child: Text(
+                'Logar',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                ),
+              ),
+              onPressed: () {},
+            ),
+          )
+        ],
+      ),
     );
   }
 }
