f4e2213ccf0c183597c6cab5eb247491e60a1122
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-03-27 12:41:19 -0300

    adicionado plugin de shared preferences para salvar a ultima aba que o usuario acessou antes de fazer logout

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:carros/drawer_list.dart';
 import 'package:carros/pages/carros_list.dart';
 import 'package:carros/service/carro_service.dart';
+import 'package:carros/utils/prefs.dart';
 
 import 'package:flutter/material.dart';
 
@@ -10,34 +11,52 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin<HomePage> {
+
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _tabController = TabController(length: 3, vsync: this);
+
+    Prefs.getInt("tabIndex").then((int tabIndex) {
+      _tabController.index = tabIndex;
+    });
+
+    _tabController.addListener(() {
+      Prefs.setInt("tabIndex", _tabController.index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Carros'),
-          bottom: TabBar(
-            tabs: [
-              Tab(
-                text: "Clássicos",
-              ),
-              Tab(
-                text: "Esportivo",
-              ),
-              Tab(
-                text: "Luxo",
-              ),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Carros'),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(
+              text: "Clássicos",
+            ),
+            Tab(
+              text: "Esportivo",
+            ),
+            Tab(
+              text: "Luxo",
+            ),
+          ],
         ),
-        body: TabBarView(children: [
-          CarrosList(TipoCarro.CLASSICO),
-          CarrosList(TipoCarro.ESPORTIVO),
-          CarrosList(TipoCarro.LUXO)
-        ]),
-        drawer: DrawerList(),
       ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+        CarrosList(TipoCarro.CLASSICO),
+        CarrosList(TipoCarro.ESPORTIVO),
+        CarrosList(TipoCarro.LUXO)
+      ]),
+      drawer: DrawerList(),
     );
   }
 }
